9065662051db5d2e819b9297a0009acd9b92a86a
“[jhanca27]” <[jhancarlohigueracarvajal@gmail.com]>
2022-09-09 18:04:09 -0500

    Completed home view
    Co-authored-by: fahida rios <fahidarios@gmail.com>
    Co-authored-by: claunicole <claunicole.gf@gmail.com>

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -1,14 +1,16 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class EmployeeTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "validation" do
+  test 'validation' do
     employee = Employee.new
-    assert_not employee.save,"saved company without name, email, position, private_number"
+    assert_not employee.save, 'saved company without name, email, position, private_number'
   end
-  test "validation number of private_number" do
-      assert_equal Integer,employees(:one).private_number.class, "comparate private number"
+  test 'validation number of private_number' do
+    assert_equal Integer, employees(:one).private_number.class, 'comparate private number'
   end
 end
