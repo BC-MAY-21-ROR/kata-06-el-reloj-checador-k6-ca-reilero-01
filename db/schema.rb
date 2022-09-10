df6bc29be9bfd9b5993be9744804930a8e625ff0
fahida rios <fahidarios@gmail.com>
2022-09-09 19:31:22 -0500

    creating the new branch and new models
    Co-authored-by: claunicole <claunicole.gf@gmail.com>
    Co-authored-by: jhanca27 <jhancarlohigueracarvajal@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_005846) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_10_002859) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attendances", force: :cascade do |t|
+    t.datetime "checkin"
+    t.datetime "checkout"
+    t.bigint "employee_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["employee_id"], name: "index_attendances_on_employee_id"
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -32,5 +41,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_005846) do
     t.index ["company_id"], name: "index_employees_on_company_id"
   end
 
+  create_table "reports", force: :cascade do |t|
+    t.integer "average_time_month"
+    t.integer "absence_month"
+    t.integer "attendance_day"
+    t.bigint "employee_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["employee_id"], name: "index_reports_on_employee_id"
+  end
+
+  add_foreign_key "attendances", "employees"
   add_foreign_key "employees", "companies"
+  add_foreign_key "reports", "employees"
 end
