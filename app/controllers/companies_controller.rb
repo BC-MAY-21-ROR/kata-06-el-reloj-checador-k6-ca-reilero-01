2831ca79d87852990ad5bf8d4d374b4729f66318
“[jhanca27]” <[jhancarlohigueracarvajal@gmail.com]>
2022-09-14 19:23:10 -0500

    create crud about company create view new.html.erb Co-authored-by: fahida rios <fahidarios@gmail.com> Co-authored-by: claunicole <claunicole.gf@gmail.com>

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,6 +2,42 @@
 
 class CompaniesController < ApplicationController
   def index
-    @companys = Company.all
+    @companies = Company.all
+  end
+
+  def show
+    #@companies = Company.find(params[:id])
+  end
+  
+  def new
+    @companies = Company.new
+  end 
+
+  def create
+    @companies = Company.create(company_params)
+    if @companies.persisted?
+      redirect_to companies_path
+    end 
+  end
+
+  def edit
+
+  end
+
+  def update
+    @companies.update(company_params)
+    if @companies.update company_params
+      redirect_to companies_path
+    end 
+  end
+
+  def delete
+    @companies.destroy
+    redirect_to companies_path
+  end
+
+  private
+  def company_params
+    params.require(:company).permit(:name,:address)
   end
 end
