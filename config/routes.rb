0d6d366477cb3d7b2461f6e29264095cbfda5fc4
“[jhanca27]” <[jhancarlohigueracarvajal@gmail.com]>
2022-09-14 19:23:10 -0500

    create crud about company create view new.html.erb Co-authored-by: fahida rios <fahidarios@gmail.com> Co-authored-by: claunicole <claunicole.gf@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@
 Rails.application.routes.draw do
   get 'companies', to: 'companies#index'
   get 'employees', to: 'employees#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'companies/new'
+  get 'companies/:id', to: 'companies#show', as:'show_company'
+  get 'companies/:id/edit', to: 'companies#edit', as:'edit_company'
+  post 'companies', to: 'companies#create'
+  delete 'companies/:id', to: 'companies#delete', as:'delete_company'
+  patch 'companies/:id', to: 'companies#update', as:'company'
 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root 'home#index'
 end
