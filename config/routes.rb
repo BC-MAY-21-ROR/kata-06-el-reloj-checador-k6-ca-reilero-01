05edf903ea6b3235d0f698f148ea14015ab3622a
fahida rios <fahidarios@gmail.com>
2022-09-16 16:57:27 -0500

    we add the new in views
    Co-authored-by: jhanca27 <jhancarlohigueracarvajal@gmail.com>
    Co-authored-by: claunicole <claunicole.gf@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'employees', to: 'employees#index'
+
+  # url employees
+  #get 'employees', to: 'employees#index'
+  #get 'employees/new'
+  #get 'employees/:id', to: 'employees#show', as:'show_employees'
+  #get 'employees/:id/edit', to: 'employees#edit', as:'edit_employee'
+  #post 'employees', to: 'employees#create'
+  #delete 'employees/:id', to: 'employees#destroy', as:'delete_employee'
+  #patch 'employees/:id', to: 'employees#update', as:'employee'
 
   resources :companies
+  resources :employees
+  
 
   root 'home#index'
 end
