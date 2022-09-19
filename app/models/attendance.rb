76965fd4ec9756fda9ab2df62c4d1c277fd781fb
“[jhanca27]” <[jhancarlohigueracarvajal@gmail.com]>
2022-09-19 16:03:32 -0500

    use rubocop

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Attendance < ApplicationRecord
   validates :checkin, uniqueness: true
   validates :checkout, uniqueness: true
--- test/models/attendance_test.rb
+++ test/models/attendance_test.rb
@@ -1,4 +1,6 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class AttendanceTest < ActiveSupport::TestCase
   test 'validation' do
