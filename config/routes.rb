dc9ebed951b86af750003370a049adf0b29c5cc5
“[jhanca27]” <[jhancarlohigueracarvajal@gmail.com]>
2022-09-19 21:25:04 -0500

    improve view index of companies and a little big of the view edit Co-authored-by: claunicole <claunicole.gf@gmail.com> Co-authored-by: fahida rios <fahidarios@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'companies', to: 'companies#index'
   get 'employees', to: 'employees#index'
-  get 'companies/new'
-  get 'companies/:id', to: 'companies#show', as:'show_company'
-  get 'companies/:id/edit', to: 'companies#edit', as:'edit_company'
-  post 'companies', to: 'companies#create'
-  delete 'companies/:id', to: 'companies#destroy', as:'delete_company'
-  patch 'companies/:id', to: 'companies#update', as:'company'
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
+  resources :companies
+
   root 'home#index'
 end
