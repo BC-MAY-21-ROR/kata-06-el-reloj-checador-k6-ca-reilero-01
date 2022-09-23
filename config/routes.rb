fb09865d495c2a6e20a1a8ee4f54a87b8451ba7a
claunicole <claunicole.gf@gmail.com>
2022-09-22 20:21:59 -0400

    Create the admin dashboard Fix authentication and administrator permissions
    Co-authored-by: jhanca27 <jhancarlohigueracarvajal@gmail.com>
    Co-authored-by: fahida0303 <fahidarios@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'admin/index'
   devise_for :users
 
   resources :companies
   resources :employees
+
+  get '/admin', to: 'admin#index'
 
   root 'home#index'
 end
