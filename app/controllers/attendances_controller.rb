7fd8d155577aa91c7816423d8a1b2d083815ce93
claunicole <claunicole.gf@gmail.com>
2022-09-28 18:31:18 -0400

    Fix: Attendance Controller and success messages
    Co-authored-by: jhanca27 <jhancarlohigueracarvajal@gmail.com>
    Co-authored-by: fahida0303 <fahidarios@gmail.com>

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -4,10 +4,6 @@ class AttendancesController < ApplicationController
   def index
     @attendances = Attendaces.all
   end
-
-  #def show
-   # @attendances = Attendance.find(params[:id])
-  #end
   
   def new
     @attendances = Attendance.new
@@ -22,64 +18,40 @@ class AttendancesController < ApplicationController
     end 
 
   end
-  def checkin
-<<<<<<< HEAD
-<<<<<<< HEAD
-    @attendance = Attendance.create(atendance_params)
-=======
-    @attendances = Attendance.create(attendances_params.merge(:checkin => Time.now))
->>>>>>> Fix: Checkin works properly with the current time
-    if @attendances.persisted?
-    redirect_to root_path
-    end
-=======
-    @attendances = Attendance.find_by(attendances_params)
-    if @attendances.nil? || @attendances.checkin.strftime("%F") == Date.today.strftime("%F") 
+
+  def set_variables
+    @find_attendances = Attendance.find_by(attendances_params)
+  end  
+
+  def check_attendance
+    set_variables.nil? || set_variables.checkin.strftime("%F") != Date.today.strftime("%F") 
+
+  end  
+
+  def checkin 
+    if check_attendance 
       @attendances = Attendance.create(attendances_params.merge(:checkin => Time.now))
       if @attendances.persisted?
-        redirect_to root_path
+        redirect_to root_path, :notice => "Checkin realizado correctamente"
       end
     else
-  
-      redirect_to root_path, :notice => "Ya hiciste el checkin"
-      
+      redirect_to root_path, :notice => "Ya hiciste el checkin"  
     end 
 >>>>>>> improve checkin and checkout
   end
 
   def checkout
-    @attendances = Attendance.find_by(attendances_params)
-    if @attendances.nil? 
+    if set_variables.nil? 
       redirect_to root_path, :notice => "Debes hacer el checkin primero"
-    elsif @attendances.checkout.nil?
-      if @attendances.update(attendances_params.merge(:checkout => Time.now))
-        redirect_to root_path
+    elsif set_variables.checkout.nil?
+      if set_variables.update(attendances_params.merge(:checkout => Time.now))
+        redirect_to root_path, :notice => "Checkout realizado correctamente"
       end
     else 
       redirect_to root_path, :notice => "Ya hiciste el checkout"
     end
   end  
 
-  #def edit
-#  @attendances = Attendance.find(params[:id])
-  #end
-
-  #def update
-   ############# @attendances = Attendance.find(params[:id])
-   ############ if @attendances.update(attendance_params)
-      #########redirect_to attendances_path
-    #########else
-   ########   render :edit, status: :unprocessable_entity
-   ####### end 
-  ######end
-
- ##### def destroy
- ####   @attendances = Attendance.find(params[:id])
- ###   @attendances.destroy
-  ##  redirect_to attendances_path,
-## ##   status: :see_other
-  #end
-#
   private
   def attendances_params
     params.require(:attendance).permit(:checkin, :checkout, :employee_id)
